1a06f64e0ddfd6006c59462550466bc93cbf4ce8
jackhein <moustiquejunior@gmail.com>
2024-05-20 19:15:31 +0200

    2024_04_27_00_new_transport.sql: - Correct correlation between Map ID of the transport and Gameobject_template

--- data/sql/db-world/base/2024_04_27_00_new_transport.sql
+++ data/sql/db-world/base/2024_04_27_00_new_transport.sql
@@ -1,6 +1,6 @@
--- Type => 15 for ships, 11 for elevators ; Data0 => TaxiPath ID ; Data1 => Speed ; Data2 => Accelerator ; Data5 => ? ; Data6 => Shipmen group ID ; Data7 => ? ; Data8 => ?
+-- Type => 15 motion, 11 static; Data0 => TaxiPathNode ID; Data1 => Speed; Data2 => Accelerator; Data5 => ? ; Data6 => Transport Map ID ; Data7 => ? ; Data8 => ?
 INSERT INTO `gameobject_template` (`entry`,`type`,`displayId`,`name`,`size`,`Data0`,`Data1`,`Data2`,`Data5`,`Data6`,`Data7`,`Data8`,`AIName`,`ScriptName`,`VerifiedBuild`)
-VALUES ('401010', '15', '6637', 'Darkwaves', '1', '2000', '30', '1', '1', '0', '0', '0', '', '', '12340');
+VALUES ('401010', '15', '6637', 'Darkwaves', '1', '2000', '30', '1', '1', '725', '0', '0', '', '', '12340');
 
 INSERT INTO `transports` (`entry`, `name`)
 VALUES ('401010', 'Shadowprey, Desolace and Revanstuck, The Hinterlands (Boat, Horde ("Shadowstream")')
