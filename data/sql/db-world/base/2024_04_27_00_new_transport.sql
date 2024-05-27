6b80dc0eff329276119aeebd2238b8f42e1a2e2d
jackhein <moustiquejunior@gmail.com>
2024-05-27 18:41:43 +0200

    2024_04_27_00_new_transport.sql: Add flags for new transport entry in gameobject_template_addon

--- data/sql/db-world/base/2024_04_27_00_new_transport.sql
+++ data/sql/db-world/base/2024_04_27_00_new_transport.sql
@@ -2,5 +2,8 @@
 INSERT INTO `gameobject_template` (`entry`,`type`,`displayId`,`name`,`size`,`Data0`,`Data1`,`Data2`,`Data5`,`Data6`,`Data7`,`Data8`,`AIName`,`ScriptName`,`VerifiedBuild`)
 VALUES ('401010', '15', '6637', 'Darkwaves', '1', '2000', '30', '1', '1', '725', '0', '0', '', '', '12340');
 
+INSERT INTO `gameobject_template_addon` (`entry`, `flags`)
+VALUES ('401010', '40');
+
 INSERT INTO `transports` (`entry`, `name`)
-VALUES ('401010', 'Shadowprey, Desolace and Revanstuck, The Hinterlands (Boat, Horde ("Shadowstream")')
+VALUES ('401010', 'Shadowprey, Desolace and Revanstuck, The Hinterlands (Boat, Horde ("Shadowstream")');
